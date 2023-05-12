0704d16a5b0e9fbedf2b2fb284c4f59bcfaabac6
Yohannes Lilay <yohannesLilay@gmail.com>
2023-05-12 09:34:01 +0300

    feat: setting environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  version: '0.0.0',
+  baseApiUrl: 'http://localhost:3000',
+  apiProvider: '/api',
+  apiVersion: '/v1',
+  serverUrl: '',
 };
+environment.serverUrl = `${environment.baseApiUrl}${environment.apiProvider}${environment.apiVersion}`;
