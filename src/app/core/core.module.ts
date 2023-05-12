227cc2b677470f2a2f591975f6a214b92ab1c3bc
Yohannes Lilay <yohannesLilay@gmail.com>
2023-05-12 08:46:00 +0300

    fix: registering progress bar interceptor & service as a provider in the core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,8 +6,23 @@ import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 /** Custom Modules */
 import { SharedModule } from '../shared/shared.module';
 
+/** Custom Services */
+import { ProgressBarService } from './progress-bar/progress-bar.service';
+
+/** Custom Interceptors */
+import { ProgressInterceptor } from './progress-bar/progress.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 @NgModule({
   declarations: [ProgressBarComponent],
   imports: [SharedModule],
+  providers: [
+    ProgressBarService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ProgressInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {}
